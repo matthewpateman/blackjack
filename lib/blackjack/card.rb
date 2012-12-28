f0517ef41beb21d8e93b4d6054483a97237bf086
Roland Swingler <roland.swingler@gmail.com>
2012-12-28 19:21:09 +0000

    Add a method to format a card as a 2 character string.
    Notice I have to turn the suit from a symbol into as string to get at
    the first character, and I can use the same indexing syntax on a
    string as I can on an array.
    
    I've tested a few more possibilities here; you don't need to be
    exhaustive in your tsting, but this felt like it benefited from more
    assertions.

--- lib/blackjack/card.rb
+++ lib/blackjack/card.rb
@@ -6,5 +6,26 @@ module Blackjack
       @rank = rank
       @suit = suit
     end
+
+    def to_s
+      rank_letter + suit.to_s[0]
+    end
+
+    def rank_letter
+      case rank
+      when 1
+        "A"
+      when 2..9
+        rank.to_s
+      when 10
+        "T"
+      when 11
+        "J"
+      when 12
+        "Q"
+      when 13
+        "K"
+      end
+    end
   end
 end
--- test/card_test.rb
+++ test/card_test.rb
@@ -8,4 +8,11 @@ class CardTest < MiniTest::Unit::TestCase
   def test_card_has_a_rank
     assert_equal 9, Blackjack::Card.new(9, :spades).rank
   end
+
+  def test_string_formatting
+    assert_equal "9s", Blackjack::Card.new(9, :spades).to_s
+    assert_equal "Kh", Blackjack::Card.new(13, :hearts).to_s
+    assert_equal "Ad", Blackjack::Card.new(1, :diamonds).to_s
+    assert_equal "Jc", Blackjack::Card.new(11, :clubs).to_s
+  end
 end
